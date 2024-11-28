cd535dd31ae1e31145d26bceb208c9be3343ca86
Eli Zibin <elizibin@gmail.com>
2024-11-28 09:21:49 -0800

    add repoducible error

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { PropsWithChildren, useState } from "react";
+import { Canvas,  Circle, Group, Text } from "@shopify/react-native-skia";
+import { Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const MyContext = React.createContext<number>(0);
+
+type ContextProviderProps = PropsWithChildren & {
+  value: number;
+};
+const ContextProvider = (props: ContextProviderProps) => {
+  return <MyContext.Provider value={props.value}>
+    {props.children}
+  </MyContext.Provider>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const useContextProvider = () => {
+  return React.useContext(MyContext);
+}
+
+const Inner = () => {
+  const value = useContextProvider()
+  const width = value;
+  const height = value;
+  const r = width * 0.33;
+
+  return (
+    <Canvas style={{ width, height }}>
+      <ContextProvider value={value}>
+        <Group blendMode="multiply">
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={value - r} cy={r} r={r} color="green" />
+          <Circle cx={value / 2} cy={value - r} r={r} color="yellow" />
+        </Group>
+      </ContextProvider>
+    </Canvas>
+  );
+}
+const App = () => {
+  const [value, setValue] = useState(256)
+  return (
+    <ContextProvider value={value}>
+      <Inner />
+      <Button title="Change Value" onPress={() => setValue(value + 10)} />
+    </ContextProvider>
+  );
+};
+export default App;
